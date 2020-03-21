02c7c98dbc5b58cb61b524dec8403492287d171b
Szymon Kruszewski <szmajchel165@gmail.com>
2020-03-21 21:17:05 +0100

    Sync local repo.

--- src/osmProject1/AppModel.java
+++ src/osmProject1/AppModel.java
@@ -6,8 +6,33 @@ public class AppModel {
 	/*Random code, just for future reference*/
 	/* Attributes */
 	private ArrayList<Patient> mList = new ArrayList<>();
+
+	
+	public AppModel()
+	{
+		
+	}
+	
+	public void addPatient(Patient p)
+	{
+		this.mList.add(p);
+	}
+	
+	public void removePatient(Patient p)
+	{
+		this.mList.remove(p);
+	}
 	
 	
+
+	
+
+	
+	
+	
+	
+	
+
 	
 	
 	
