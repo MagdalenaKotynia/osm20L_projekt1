9c3dad69a2b723f430aab12238efcf94a9fca00b
Szymon Kruszewski <szmajchel165@gmail.com>
2020-03-22 01:05:26 +0100

    Minor changes.

--- src/osmProject1/AppModel.java
+++ src/osmProject1/AppModel.java
@@ -1,5 +1,24 @@
 package osmProject1;
+import java.util.*;
 
 public class AppModel {
+
+	private ArrayList<Patient> mList = new ArrayList<>();
+
+	
+	public AppModel()
+	{
+		
+	}
+	
+	
+	public void addPatient(String n, String s, String p, String i, String g, Examination e){
+		e=null;
+		this.mList.add(new Patient(n, s, p, i, g, e));
+	}
+	
+	public void removePatient(int index){
+		this.mList.remove(index);
+	}
 
 }
