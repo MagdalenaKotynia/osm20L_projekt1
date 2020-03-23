cebf27c7845498820091b94167db843ba2e721f2
Szymon Kruszewski <szmajchel165@gmail.com>
2020-03-23 21:59:58 +0100

    Started AppView.

--- src/osmProject1/AppView.java
+++ src/osmProject1/AppView.java
@@ -1,10 +1,82 @@
 package osmProject1;
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
 
-public class AppView {
+
+public class AppView extends JFrame {
 
 	/* Attributes */
+	
+	public AppView() {
+		
+		this.setFrame();
+		this.setGui();
+		this.setMenu();
+		this.setPatientPanel();
+		
+		
+		
+		
+		/*
+		
+		
+		
+		
+		JMenuBar menu = new JMenuBar();
+		mainPanel.add(BorderLayout.NORTH, menu);
+		
+		
+		JPanel patientPanel = new JPanel();
+		mainPanel.add(BorderLayout.WEST, patientPanel);
+		patientPanel.setBackground(Color.red);
+		patientPanel.setPreferredSize(new Dimension(300, 400));
+		
+		
+		patientPanel.setBorder(BorderFactory.createTitledBorder("Pacjent"));
+		
+		
+		*/
+	}
+	
+	private void setGui() {
+		
+		JPanel mainPanel = new JPanel();
+		this.getContentPane().add(mainPanel);
+		mainPanel.setPreferredSize(new Dimension(800, 600));
+		mainPanel.setBackground(Color.gray);
+		this.pack();
+		
+		mainPanel.setLayout(new BorderLayout());
+		
+		
+	}
+	
+	private void setFrame() {
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(800, 600);
+		this.setVisible(true);
+		this.setTitle("Rejestracja wyników badań");
+	}
+	
+	private void setMenu() {
+		
+		JMenuBar menuBar = new JMenuBar();
+		this.setJMenuBar(menuBar);
+		menuBar.setVisible(true);
+		
+		JMenu menu = new JMenu("Aplikacja");
+		menuBar.add(menu);
+		
+		JMenuItem menuItem = new JMenuItem("Zamknij");
+		// menuItem.addActionListener(this);				//add thread handling
+		menu.add(menuItem);
+	}
+	
+	private void setPatientPanel() {
+		
+	}
+	
 	
 	
 }
--- src/osmProject1/Main.java
+++ src/osmProject1/Main.java
@@ -1,13 +1,12 @@
 package osmProject1;
 
-import java.awt.*;
-import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-	
+		AppView app = new AppView();
+		
 		
 	}
 
